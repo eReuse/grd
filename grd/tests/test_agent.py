698836b7eca9b802597ab2c2446473903d568d9d
santiago <santiago@pangea.org>
2015-06-29 15:56:10 +0200

    Unit test for Agent.get_absolute_url

--- grd/tests/test_agent.py
+++ grd/tests/test_agent.py
@@ -8,3 +8,7 @@ class AgentTest(TestCase):
     def test_agent_representation(self):
         for agent in Agent.objects.all():
             self.assertIsNotNone(repr(agent))
+    
+    def test_get_absolute_url(self):
+        agent = Agent.objects.first()
+        self.assertEqual(agent.get_absolute_url(), '/api/agents/%d/' % (agent.pk,))
