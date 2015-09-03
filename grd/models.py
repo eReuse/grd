71954b484194e93bbf7f20307988f2f35e17d05f
santiago <santiago@pangea.org>
2015-09-03 10:35:58 +0200

    Define AgentUser repr & unittests.

--- grd/models.py
+++ grd/models.py
@@ -169,6 +169,9 @@ class Agent(models.Model):
 class AgentUser(models.Model):
     url = models.URLField('URL pointing to an User or an Organization.',
                           max_length=128, unique=True)
+    
+    def __str__(self):
+        return self.url
 
 
 class Location(gis_models.Model):
--- grd/tests/test_agent.py
+++ grd/tests/test_agent.py
@@ -1,5 +1,7 @@
+import unittest
+
 from django.test import TestCase
-from grd.models import Agent
+from grd.models import Agent, AgentUser
 
 
 class AgentTest(TestCase):
@@ -15,3 +17,22 @@ class AgentTest(TestCase):
             agent.get_absolute_url(),
             '/api/agents/%d/' % agent.pk
         )
+
+
+class AgentUserTest(TestCase):
+
+    def test_representation(self):
+        obj = AgentUser(url="http://example.org/user/alice")
+        self.assertIn(obj.url, repr(obj))
+    
+    @unittest.skip("Do we really want to normalize the URLs?")
+    # XXX maybe trying to normalize it may cause more trouble
+    # than helping us to avoid duplicate info.
+    def test_cannonical_url(self):
+        # Avoid different instances with equivalent URLS:
+        from django.core.exceptions import ValidationError
+        AgentUser.objects.create(url="http://example.org/user/alice")
+        a = AgentUser(url="http://example.org/user/alice/")
+        self.assertRaises(ValidationError, a.full_clean)
+        # "https://example.org/user/alice"
+        # "https://example.org/user/alice/"
