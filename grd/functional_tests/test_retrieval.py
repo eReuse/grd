a894f85e8cae9ccd9cc45ed800bd7bbe0f7321b8
santiago <santiago@pangea.org>
2015-09-08 13:57:33 +0200

    Adapt tests to new permissions.
    Fix regression 73eec4bc8fc3c3a1381ea98d70f66b69f1aa3d7f

--- grd/functional_tests/test_retrieval.py
+++ grd/functional_tests/test_retrieval.py
@@ -1,8 +1,30 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from rest_framework.test import APILiveServerTestCase
 
+from .common import BaseTestCase
+
 
 class ApiTest(StaticLiveServerTestCase, APILiveServerTestCase):
+    fixtures = ['agents.json', 'devices.json', 'events.json', 'users.json']
+    
+    def test_retrieve_api_base(self):
+        response = self.client.get('/api/')
+        self.assertEqual(200, response.status_code)
+    
+    def test_retrieve_device_list(self):
+        response = self.client.get('/api/devices/')
+        self.assertEqual(403, response.status_code)
+    
+    def test_retrieve_event_list(self):
+        response = self.client.get('/api/events/')
+        self.assertEqual(403, response.status_code)
+    
+    def test_retrieve_event_detail(self):
+        response = self.client.get('/api/events/1/')
+        self.assertEqual(403, response.status_code)
+
+
+class ApiSuperuserTest(BaseTestCase):
     fixtures = ['agents.json', 'devices.json', 'events.json', 'users.json']
     
     def test_retrieve_api_base(self):
